d9dfed47501064b2c7d073fd2cc062c81dd72520
Vaibhav <23je0271@gmail.com>
2025-02-28 15:51:54 +0530

    masked mode added

--- transformers/attention.py
+++ transformers/attention.py
@@ -2,8 +2,9 @@ import torch
 from torch import nn
 
 class multi_head_attention(nn.Module):
-    def __init__(self,no_of_heads,embeddings_dims=512):
+    def __init__(self,no_of_heads,embeddings_dims=512,Masked=False):
         super().__init__( )
+        self.masked = Masked
         self.heads = no_of_heads
         self.head_dims = embeddings_dims// no_of_heads
         self.embeddings_dims = embeddings_dims
@@ -17,7 +18,6 @@ class multi_head_attention(nn.Module):
         K = self.k(X)
         Q = self.q(X)
         V = self.v(X)
-        print(K.shape)
         K = K.reshape(batch_size,sequence_length,self.heads,self.head_dims)
         Q = Q.reshape(batch_size,sequence_length,self.heads,self.head_dims)
         V = V.reshape(batch_size,sequence_length,self.heads,self.head_dims)
@@ -26,7 +26,11 @@ class multi_head_attention(nn.Module):
         V = V.permute(0, 2, 1, 3)
         KT = torch.transpose(K,-1,-2)
         Attention_matrix = torch.divide(torch.matmul(Q,KT),self.head_dims**0.5)
+        if self.masked:
+            Attention_matrix = torch.tril(Attention_matrix)
+            Attention_matrix[Attention_matrix == 0] = -torch.inf
         Attention_matrix = torch.softmax(Attention_matrix,dim = -1)
+        print(Attention_matrix)
         contextual_embeddings = torch.matmul(Attention_matrix,V)
         contextual_embeddings = contextual_embeddings.reshape(batch_size,sequence_length,self.embeddings_dims)
         contextual_embeddings = self.transform(contextual_embeddings)
